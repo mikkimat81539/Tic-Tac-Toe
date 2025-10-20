a577936a2720b76dbb8e253d609841693621b4ea
Mikaela Matthews <mikaelamatthews@MacBookAir.lan>
2025-10-19 19:19:15 -0500

    grid created

--- game.py
+++ game.py
@@ -10,7 +10,14 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Sname Game")
 
 # Create 3X3 grid
-lines1 = Surface(150, 60, 30, 100, '#027559')
+vert1 = Surface(200, 60, 10, 200, '#027559')
+vert2 = Surface(290, 60, 10, 200, '#027559')
+
+hort1 = Surface(150, 110, 200, 10, '#027559')
+hort2 = Surface(150, 190, 200, 10, '#027559')
+
+# Create player (X)
+
 
 # Game loop
 running = True
@@ -25,7 +32,11 @@ while running:
             running = False
 
     # Render Game Here
-    screen.blit(screen, lines1.game_rects(screen))
+    vert1.game_rects(screen)
+    vert2.game_rects(screen)
+
+    hort1.game_rects(screen)
+    hort2.game_rects(screen)
 
     # Flip game
     pygame.display.flip()
